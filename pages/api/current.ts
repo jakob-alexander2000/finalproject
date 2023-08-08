5f55f7e8312d7d6b5d8e78051baae602dead2b1d
Jakob Alexander <jeilinghoff@googlemail.com>
2023-08-08 11:47:41 +0200

    prisma local changes

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(200).json(currentUser);
   } catch (error) {
     console.log(error);
+    
     return res.status(400).end();
   }
 }
